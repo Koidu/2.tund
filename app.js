3fb91b75151121c0375ddf20f3a86db51ac1a34c
Koidu Kannela <koidkan@greeny.cs.tlu.ee>
2016-02-10 11:11:13 +0200

    tunnitoo

--- app.js
+++ app.js
@@ -9,12 +9,40 @@
     }
     Moosipurk.instance = this; //this viitab moosipurgile
 
-    console.log('moosipurgi sees');
+    console.log(this);
+    //console.log('moosipurgi sees');
+
+    //kõik muutujad, mis on üldised ja muudetavad
+    this.click_count = 0;
+
+
+
+    //panen rakenduse tööle
+    this.init();
   };
 
-window.onload = function(){
-  var app = new Moosipurk();
-};
+  // kõik moosipurgi funktsioonid tulevad siia sisse
+  Moosipurk.prototype = {
+    init: function(){
+      console.log('rakendus käivitus');
+      this.bindMouseEvents();
+    },   //esimene käivitus. siin kuulame hiireklõpse. esialgne loogika
+
+    bindMouseEvents: function(){
+      document.querySelector('.add-new-jar').addEventListener('click', this.addNewClic.bind(this));
+    },
+
+    addNewClic: function(event){
+      //console.log(event);
+      this.click_count++;
+      console.log(this_click_count);
+
+    }  //kui toimub hiireklõps, siis käivitub funktsioon
+  };
+
+  window.onload = function(){
+    var app = new Moosipurk();
+  };
 
 
 
